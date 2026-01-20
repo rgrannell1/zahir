7bcf352b9fb91ec755eb8cc3fd80af1e0ac4c388
Róisín Grannell <r.grannell2@gmail.com>
2026-01-20 19:43:13 +0000

    Types n docs

--- zahir/jobs/empty.py
+++ zahir/jobs/empty.py
@@ -1,7 +1,10 @@
+from decimal import Context
+from typing import Generator
+from zahir.dependencies.group import DependencyGroup
 from zahir.jobs.decorator import spec
 
 
 @spec()
-def Empty(context, args, dependencies):
-    """A job that does nothing. Useful if you need to block on a dependency, but not do anything"""
+def Empty(context: Context, args: None, dependencies: DependencyGroup) -> Generator[None, None, None]:
+    """A job that does nothing. Useful if you need to block on a dependency, but not do anything when it's satisfied."""
     yield from []
